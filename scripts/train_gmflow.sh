c05e2e832f0d6ef0b6b843ee3283f87cec43bde8
xxxxcj <1014212816@qq.com>
2023-06-26 11:48:48 +0000

    style: revise sh file to match download datsens

--- scripts/train_gmflow.sh
+++ scripts/train_gmflow.sh
@@ -5,7 +5,7 @@
 # number of gpus for training, please set according to your hardware
 # by default use all gpus on a machine
 # can be trained on 4x 16GB V100 or 2x 32GB V100 or 2x 40GB A100 gpus
-NUM_GPUS=4
+NUM_GPUS=1
 
 # chairs
 CHECKPOINT_DIR=checkpoints/chairs-gmflow && \
@@ -13,7 +13,7 @@ mkdir -p ${CHECKPOINT_DIR} && \
 python -m torch.distributed.launch --nproc_per_node=${NUM_GPUS} --master_port=9989 main.py \
 --launcher pytorch \
 --checkpoint_dir ${CHECKPOINT_DIR} \
---batch_size 16 \
+--batch_size 12 \
 --val_dataset chairs sintel kitti \
 --lr 4e-4 \
 --image_size 384 512 \
@@ -25,25 +25,25 @@ python -m torch.distributed.launch --nproc_per_node=${NUM_GPUS} --master_port=99
 --num_steps 100000 \
 2>&1 | tee -a ${CHECKPOINT_DIR}/train.log
 
-# things (our final model is trained for 800K iterations, for ablation study, you can train for 200K)
-CHECKPOINT_DIR=checkpoints/things-gmflow && \
-mkdir -p ${CHECKPOINT_DIR} && \
-python -m torch.distributed.launch --nproc_per_node=${NUM_GPUS} --master_port=9989 main.py \
---launcher pytorch \
---checkpoint_dir ${CHECKPOINT_DIR} \
---resume checkpoints/chairs-gmflow/step_100000.pth \
---stage things \
---batch_size 8 \
---val_dataset things sintel kitti \
---lr 2e-4 \
---image_size 384 768 \
---padding_factor 16 \
---upsample_factor 8 \
---with_speed_metric \
---val_freq 40000 \
---save_ckpt_freq 50000 \
---num_steps 800000 \
-2>&1 | tee -a ${CHECKPOINT_DIR}/train.log
+# # things (our final model is trained for 800K iterations, for ablation study, you can train for 200K)
+# CHECKPOINT_DIR=checkpoints/things-gmflow && \
+# mkdir -p ${CHECKPOINT_DIR} && \
+# python -m torch.distributed.launch --nproc_per_node=${NUM_GPUS} --master_port=9989 main.py \
+# --launcher pytorch \
+# --checkpoint_dir ${CHECKPOINT_DIR} \
+# --resume checkpoints/chairs-gmflow/step_100000.pth \
+# --stage things \
+# --batch_size 8 \
+# --val_dataset things sintel kitti \
+# --lr 2e-4 \
+# --image_size 384 768 \
+# --padding_factor 16 \
+# --upsample_factor 8 \
+# --with_speed_metric \
+# --val_freq 40000 \
+# --save_ckpt_freq 50000 \
+# --num_steps 800000 \
+# 2>&1 | tee -a ${CHECKPOINT_DIR}/train.log
 
 # sintel
 CHECKPOINT_DIR=checkpoints/sintel-gmflow && \
@@ -51,7 +51,7 @@ mkdir -p ${CHECKPOINT_DIR} && \
 python -m torch.distributed.launch --nproc_per_node=${NUM_GPUS} --master_port=9989 main.py \
 --launcher pytorch \
 --checkpoint_dir ${CHECKPOINT_DIR} \
---resume checkpoints/things-gmflow/step_800000.pth \
+--resume checkpoints/chairs-gmflow/step_100000.pth \
 --stage sintel \
 --batch_size 8 \
 --val_dataset sintel kitti \
--- scripts/train_gmflow_with_refine.sh
+++ scripts/train_gmflow_with_refine.sh
@@ -29,29 +29,29 @@ python -m torch.distributed.launch --nproc_per_node=${NUM_GPUS} --master_port=99
 --num_steps 100000 \
 2>&1 | tee -a ${CHECKPOINT_DIR}/train.log
 
-# things (our final model is trained for 800K iterations, for ablation study, you can train for 200K)
-CHECKPOINT_DIR=checkpoints/things-gmflow_with_refine && \
-mkdir -p ${CHECKPOINT_DIR} && \
-python -m torch.distributed.launch --nproc_per_node=${NUM_GPUS} --master_port=9989 main.py \
---launcher pytorch \
---checkpoint_dir ${CHECKPOINT_DIR} \
---resume checkpoints/chairs-gmflow_with_refine/step_100000.pth \
---stage things \
---batch_size 8 \
---val_dataset things sintel kitti \
---lr 2e-4 \
---image_size 384 768 \
---padding_factor 32 \
---upsample_factor 4 \
---num_scales 2 \
---attn_splits_list 2 8 \
---corr_radius_list -1 4 \
---prop_radius_list -1 1 \
---with_speed_metric \
---val_freq 40000 \
---save_ckpt_freq 50000 \
---num_steps 800000 \
-2>&1 | tee -a ${CHECKPOINT_DIR}/train.log
+# # things (our final model is trained for 800K iterations, for ablation study, you can train for 200K)
+# CHECKPOINT_DIR=checkpoints/things-gmflow_with_refine && \
+# mkdir -p ${CHECKPOINT_DIR} && \
+# python -m torch.distributed.launch --nproc_per_node=${NUM_GPUS} --master_port=9989 main.py \
+# --launcher pytorch \
+# --checkpoint_dir ${CHECKPOINT_DIR} \
+# --resume checkpoints/chairs-gmflow_with_refine/step_100000.pth \
+# --stage things \
+# --batch_size 8 \
+# --val_dataset things sintel kitti \
+# --lr 2e-4 \
+# --image_size 384 768 \
+# --padding_factor 32 \
+# --upsample_factor 4 \
+# --num_scales 2 \
+# --attn_splits_list 2 8 \
+# --corr_radius_list -1 4 \
+# --prop_radius_list -1 1 \
+# --with_speed_metric \
+# --val_freq 40000 \
+# --save_ckpt_freq 50000 \
+# --num_steps 800000 \
+# 2>&1 | tee -a ${CHECKPOINT_DIR}/train.log
 
 # sintel
 CHECKPOINT_DIR=checkpoints/sintel-gmflow_with_refine && \
